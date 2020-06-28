718b444c3b3acacd14378d070b87b2d175a0c07c
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 22:21:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,14 @@
 #end
 
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-def my_collect(array) do |name|
-  name.split(" ").first
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#def my_collect(array) do |name|
+#  name.split(" ").first
+#end
+#end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
-end
+ 
