578ea178e22fcb948fb31d4e25d2f18df7c1e9ab
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 22:22:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,31 @@
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#def my_collect(array) do |name|
+#  name.split(" ").first
+#end
 
 
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#def my_collect(array) do |name|
+#  name.split(" ").first
+#end
+#end
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+#my_collect(list) {|i| i.split(" ").first}
+#
+#collection = ['ruby', 'javascript', 'python', 'objective-c']
+#my_collect(collection) do |lang|
+#  lang.upcase
+#end
+ 
