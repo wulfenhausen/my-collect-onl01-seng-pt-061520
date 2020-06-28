3a0c273c0d0a7049af9fd13e9001a8f41195b66d
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 22:22:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,6 +10,20 @@
 #end
 #end
 
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
+
 collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
